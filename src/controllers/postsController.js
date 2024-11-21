3f9370e772c655f409cdcce278124d66da69db21
Hayasshida <pedrohayashida9@gmail.com>
2024-11-21 16:20:56 -0300

    feat: upload image

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,41 @@
-import { getAllPosts } from "../models/postsModel.js";
+import fs from "fs";
+import { getAllPosts, criarPost } from "../models/postsModel.js";
 
 export async function listarPosts(req, res) {
     const resultados = await getAllPosts();
     res.status(200).json(resultados)
 }
+
+export async function postarNovoPost(req, res){
+    const novoPost = req.body;
+
+    try {  
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({
+            error: "Falha na requisição",
+        });
+    };
+};
+
+export async function uploadImagem(req, res){
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: "",
+    };
+
+    try {  
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagemAtualizada);
+        res.status(200).json(postCriado);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({
+            error: "Falha na requisição",
+        });
+    };
+};
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -5,5 +5,13 @@ const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 export async function getAllPosts(){
     const db = conexao.db("imersão-instabytes");
     const colecao = db.collection("posts");
+
     return colecao.find().toArray();
-}  
+};
+
+export async function criarPost(novoPost){
+    const db = conexao.db("imersão-instabytes");
+    const colecao = db.collection("posts");
+    
+    return colecao.insertOne(novoPost);
+}
